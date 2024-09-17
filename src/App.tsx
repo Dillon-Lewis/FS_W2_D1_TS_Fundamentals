feaec0661d1fd02b807dd299de34bec4af5798ea
Dillon-Lewis <dilllewis42@gmail.com>
2024-09-17 14:20:34 -0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <Problem1 />
       <ColorList />
+      
     </>
   )
 }
--- src/components/P2-Task2.tsx
+++ src/components/P2-Task2.tsx
@@ -1,7 +1,8 @@
 
 const filterNumbers = (number: number[]) => {
     let evens = [];
-    for (let i = number; ; i++) {
+
+    for (let i = 0; i <= number.length - 1; i++) {
         if(i % 2 === 0 ){
         evens.push(i)
         }
@@ -9,8 +10,6 @@ const filterNumbers = (number: number[]) => {
     return evens
 }
 
-
-
 let numbers: number[] = [2, 3, 67, 6, 5, 9, 1];
 
-filterNumbers(numbers)
+console.log(filterNumbers(numbers))
